43d85c70e77dcf3dab9de5f38b2970e6a6c08e87
Brendon Posen <brendon.posen@payfast.co.za>
2017-05-04 10:07:03 +0200

    update

--- wp-content/plugins/gravityformspayfast/payfast.php
+++ wp-content/plugins/gravityformspayfast/payfast.php
@@ -10,21 +10,9 @@ Text Domain: gravityformspayfast
 Domain Path: /languages
 
 ------------------------------------------------------------------------
-Copyright 2009-2015 PayFast
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+Copyright (c) 2008 PayFast (Pty) Ltd
+You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
 */
 
 
